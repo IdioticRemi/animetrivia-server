843b37cc464bc7a1445da85303754c42ed245768
Kizuru <fr.kizuru@gmail.com>
2020-01-12 17:03:55 +0100

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: "bamboo",
+  rules: {
+    "linebreak-style": 0,
+    "no-eval": 0,
+    "radix": 0,
+
+    "@typescript-eslint/ban-ts-ignore": 0,
+  },
+  parserOptions: {
+    project: "./tsconfig.json"
+  }
+};
---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import 'module-alias/register';
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { scan, Stats } from 'fs-nextra';
+
+// Application
+const app = express();
+
+// Middlewares
+app.use(bodyParser.json());
+app.use(cors());
+
+// Register Routes
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+scan('./dist/routes', { filter: (stats: Stats, path: string) => !stats.isDirectory() && (path.endsWith('.ts') || path.endsWith('.js')) }).then(async routes => {
+	[...routes.keys()].forEach(async path => {
+		const route = path.split('\\routes')[1].split('.js')[0].replace(/\\+/g, '/');
+
+		const routeFile = await import(path);
+
+		app.use(route, routeFile.default);
+	});
+
+	// Start
+	const port = process.env.PORT || 7070;
+
+	app.listen(port, () => console.log(`Server started on port ${port}`));
+});
