dda8bb18af52970222c26c9ee8759a143ddd3925
Kizuru <fr.kizuru@gmail.com>
2020-01-12 17:26:37 +0100

    Added route /api/play

--- src/routes/api/play.ts
+++ src/routes/api/play.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import ytdl from 'ytdl-core';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-	res.end('Wesh gros');
+	if (!('videoID' in req.query)) return res.status(400).end('Unknown video, please add "videoID" to query params!');
+
+	const url = `https://www.youtube.com/watch?v=${req.query.videoID}`;
+
+	try {
+		console.log(ytdl.getBasicInfo(url));
+		ytdl(url, { filter: 'audioonly', quality: 'highestaudio' }).pipe(res);
+	} catch (e) {
+		console.error(e);
+	}
 });
 
 export default router;
